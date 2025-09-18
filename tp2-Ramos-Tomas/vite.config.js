228a57334d1f754365af88a1b68af1a68f05de01
Tomás Ramos <108686460+xRamex@users.noreply.github.com>
2025-09-17 22:23:05 -0300

    Delete tp2-Ramos-Tomas directory

--- tp2-Ramos-Tomas/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/TP2_RAMOS_TOMAS_LENG_4/", // 👈 nombre EXACTO de tu repo
-});
